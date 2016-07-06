20bca72219062070c7e7ebb597cd942f0bfe0470
Tyson Bailey <onaclov2000+github@gmail.com>
2016-07-05 21:24:55 -0600

    Update ffmpeg.py

--- ffmpeg.py
+++ ffmpeg.py
@@ -23,25 +23,31 @@ class ffmpeg:
       self.framerate = self.getFrameRate(self.filename)
       if self.framerate is None:
             raise ValueError("Can't find framerate")
-      print self.framerate
       
     def dumpFrames(self, path):
       self.path = path
       try:
          subprocess.check_output(['ffmpeg -i ' + self.filename + ' ' + path + '/Pictures%d.jpg'], stderr=subprocess.STDOUT,shell=True)
       except subprocess.CalledProcessError as e:
+            print 'dumpFrames'
             print e.output
       
     def extractAudio(self,output_filename_and_path):
       # Based on this: ffmpeg -i Video.mpg -q:a 0 -map a audio.mp3
       self.audio_file = output_filename_and_path
-      subprocess.check_output(['ffmpeg -i ' + self.filename + '-q:a 0 -map a ' + output_filename_and_path], stderr=subprocess.STDOUT,shell=True)
+      try:
+         print 'ffmpeg -i ' + self.filename + '-q:a 0 -map a ' + output_filename_and_path
+         subprocess.check_output(['ffmpeg -i ' + self.filename + ' -q:a 0 -map a ' + output_filename_and_path], stderr=subprocess.STDOUT,shell=True)
       except subprocess.CalledProcessError as e:
+            print 'extractAudio'
             print e.output
     
     def combineFrames(self, output_filename_and_path):
-      print output_filename_and_path
-      subprocess.check_output(['ffmpeg', '-framerate', self.framerate, '-i', self.path + 'Pictures%d.jpg', '-i',
-                                self.audio_file, '-c:v', 'libx264', '-profile:v', 'high', '-crf', '20', '-pix_fmt',
-                                'yuv420p', output_filename_and_path])
+      print 'ffmpeg -framerate ' + self.framerate + ' -i ' + self.path + '/Pictures%d.jpg' + ' -i ' + self.audio_file + ' -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p ' + output_filename_and_path
+      try:
+         subprocess.check_output(['ffmpeg -framerate ' + self.framerate + ' -i ' + self.path + '/Pictures%d.jpg' + ' -i ' +
+                                self.audio_file + ' -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p ' + output_filename_and_path], stderr=subprocess.STDOUT,shell=True)
+      except subprocess.CalledProcessError as e:
+            print 'combineFrames'
+            print e.output
 
